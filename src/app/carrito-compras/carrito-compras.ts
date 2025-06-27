ad6133c2aa4ab334afe14ca51f29e6836a2832f7
Josefina <josefinabelaunzaran0106@gmail.com>
2025-06-27 10:26:28 -0300

    modifico carrito de compras

--- src/app/carrito-compras/carrito-compras.ts
+++ src/app/carrito-compras/carrito-compras.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DecoCarrito } from '../deco-carrito';
+import { Deco } from '../deco-list/Deco';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-carrito-compras',
   standalone: false,
   templateUrl: './carrito-compras.html',
-  styleUrl: './carrito-compras.scss'
+  styleUrls: ['./carrito-compras.scss']
 })
-export class CarritoCompras {
+export class CarritoCompras implements OnInit, OnDestroy {
 
+  lista: Deco[] = [];
+  private subscription!: Subscription;
+
+  constructor(private decoCarrito: DecoCarrito) {}
+
+  ngOnInit(): void {
+    this.subscription = this.decoCarrito.lista.subscribe(carrito => {
+      this.lista = carrito;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+  vaciarCarrito(): void {
+  this.decoCarrito.vaciarCarrito();
+}
+get total(): number {
+  return this.lista.reduce((acc, item) => acc + item.precio * item.cantidad, 0);
+}
 }
